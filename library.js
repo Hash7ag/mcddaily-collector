0565c927ae9d7acd3479118c212c487fe4ea24fe
Hash7ag <Hash7ag@protonmail.com>
2020-07-21 00:08:08 +0800

    fix(md5): upgrade md5 function to last version

--- library.js
+++ library.js
@@ -35,10 +35,47 @@ function apiRequest(token, url) {
   }
 }
 
-/* MD5 function for GAS(GoogleAppsScript) */
-/* Reference: https://gist.github.com/KEINOS/78cc23f37e55e848905fc4224483763d */
+/**
+ * ------------------------------------------
+ *   MD5 function for GAS(GoogleAppsScript)
+ *
+ * You can get a MD5 hash value and even a 4digit short Hash value of a string.
+ * ------------------------------------------
+ * Usage1:
+ *   `=MD5("YourStringToHash")`
+ *     or
+ *   `=MD5( A1 )` with the same string at A1 cell
+ *   result:
+ *     `FCE7453B7462D9DE0C56AFCCFB756193`.
+ *     For your sure-ness you can verify it in your terminal as below.
+ *     `$ md5 -s "YourStringToHash"`
+ * Usage2:
+ *   `=MD5("YourStringToHash", true)` for short Hash
+ *    result:
+ *     `6MQH`
+ *     Note that it has more conflict probability.
+ *
+ * How to install:
+ *   Copy the scipt, pase it at [Tools]-[Script Editor]-[<YourProject>]
+ *   or go https://script.google.com and paste it.
+ *   For more details go:
+ *     https://developers.google.com/apps-script/articles/
+ * Latest version:
+ *   https://gist.github.com/dotysan/36b99217fdc958465b62f84f66903f07
+ * Author:
+ *   KEINOS @ https://github.com/keinos
+ * Reference and thanks to:
+ *   https://stackoverflow.com/questions/7994410/hash-of-a-cell-text-in-google-spreadsheet
+ * ------------------------------------------
+ *
+ * @param {(string|Bytes[])} input The value to hash.
+ * @param {boolean} isShortMode Set true for 4 digit shortend hash, else returns usual MD5 hash.
+ * @return {string} The hashed input
+ * @customfunction
+ *
+ */
 function MD5(input, isShortMode) {
-  var isShortMode = !!isShortMode; // Be sure to be bool
+  var isShortMode = (isShortMode == true) ? true : false;
   var txtHash = '';
   var rawHash = Utilities.computeDigest(
     Utilities.DigestAlgorithm.MD5,
